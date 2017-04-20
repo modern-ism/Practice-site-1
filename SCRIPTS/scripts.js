4315da82466c3485e37e5063e39ecb0090d33ae1
Ethan Brown <ethan.g.brown@gmail.com>
2017-04-20 14:26:56 -0500

    Took out practice code and content

--- SCRIPTS/scripts.js
+++ SCRIPTS/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	// all custom Jquery will go here
 	// This is just a practice call
-	$("#demo").html("I think yr cute.");
+	// $("#demo").html("I think yr cute.");
 	//This is calling in the lettering .js so I can curve the text
 	$("h1").lettering();
 });
